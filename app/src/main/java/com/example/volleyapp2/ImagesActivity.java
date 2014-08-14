d775eb701de56ce7b019e92800aa181520df54b7
Chris Blunt <chris@plymouthsoftware.com>
2014-08-14 10:48:26 +0100

    Add call to remote resource.

--- app/src/main/java/com/example/volleyapp2/ImagesActivity.java
+++ app/src/main/java/com/example/volleyapp2/ImagesActivity.java
@@ -1,6 +1,7 @@
 package com.example.volleyapp2;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
@@ -8,6 +9,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 
 public class ImagesActivity extends ActionBarActivity {
@@ -16,7 +25,7 @@ public class ImagesActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
-        if (savedInstanceState == null) {
+        if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new ImagesFragment())
                     .commit();
@@ -37,7 +46,7 @@ public class ImagesActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -55,12 +64,40 @@ public class ImagesActivity extends ActionBarActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_images, container, false);
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_images, container, false);
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
 
             mAdapter = new ImageRecordsAdapter(getActivity());
 
-            return rootView;
+            ListView listView = (ListView) getView().findViewById(R.id.list1);
+            listView.setAdapter(mAdapter);
+
+            fetch();
+        }
+
+        private void fetch() {
+            JsonObjectRequest request = new JsonObjectRequest(
+                    "http://cblunt.github.io/blog-android-volley/response2.json",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
+                            // TODO Parse the response
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            Toast.makeText(getActivity(), "Unable to fetch data: " + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            VolleyApplication.getInstance().getRequestQueue().add(request);
         }
     }
 }
---
+++ app/src/main/java/com/example/volleyapp2/VolleyApplication.java
@@ -0,0 +1,29 @@
+package com.example.volleyapp2;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyApplication extends Application {
+    private static VolleyApplication sInstance;
+
+    private RequestQueue mRequestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        sInstance = this;
+    }
+
+    public synchronized static VolleyApplication getInstance() {
+        return sInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return mRequestQueue;
+    }
+}
